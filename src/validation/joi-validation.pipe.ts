a73d785c59d0cc979f0953698ce20223192c096d
Erfan <erfanminaei9@gmail.com>
2026-02-09 16:28:00 +0330

    Add Joi validation pipe

---
+++ src/validation/joi-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { ObjectSchema } from 'joi';
+
+@Injectable()
+export class JoiValidationPipe implements PipeTransform {
+  constructor(private schema: ObjectSchema) {}
+
+  transform(value: any) {
+    const { error, value: validatedValue } = this.schema.validate(value, {
+      abortEarly: false,
+    });
+
+    if (error) {
+      const errorMessages = error.details.map((detail) => detail.message).join(', ');
+      throw new BadRequestException(`Validation failed: ${errorMessages}`);
+    }
+
+    return validatedValue;
+  }
+}
