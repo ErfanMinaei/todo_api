c7f972149798b681a4633187286355ab499d5ef7
Erfan <erfanminaei9@gmail.com>
2026-02-10 16:08:42 +0330

    protect resolvers with GqlAuthGuard and use CurrentUser decorator

--- src/todo-lists/todo-lists.resolver.ts
+++ src/todo-lists/todo-lists.resolver.ts
@@ -1,27 +1,34 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 import { TodoListsService } from './todo-lists.service';
 import { CreateTodoListInput } from '../graphql';
 import { JoiValidationPipe } from '../validation/joi-validation.pipe';
 import { createTodoListSchema } from '../validation/schemas';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { CurrentUser } from '../auth/current-user.decorator';
 
 @Resolver('TodoList')
 export class TodoListsResolver {
-  constructor(private todoListsService: TodoListsService) { }
+  constructor(private todoListsService: TodoListsService) {}
 
   @Query('todoLists')
-  async todoLists(@Args('userId') userId: number) {
-    return this.todoListsService.findByUser(userId);
+  @UseGuards(GqlAuthGuard)
+  async todoLists(@CurrentUser() user: any) {
+    return this.todoListsService.findByUser(user.id);
   }
 
   @Query('todoList')
+  @UseGuards(GqlAuthGuard)
   async todoList(@Args('id') id: number) {
     return this.todoListsService.findOne(id);
   }
 
   @Mutation('createTodoList')
+  @UseGuards(GqlAuthGuard)
   async createTodoList(
     @Args('input', new JoiValidationPipe(createTodoListSchema)) input: CreateTodoListInput,
+    @CurrentUser() user: any,
   ) {
-    return this.todoListsService.create(input);  //will fix this error after applying gurads 
+    return this.todoListsService.create(input, user.id);
   }
 }
--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,24 +1,29 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoInput, UpdateTodoInput } from '../graphql';
 import { JoiValidationPipe } from '../validation/joi-validation.pipe';
 import { createTodoSchema, updateTodoSchema } from '../validation/schemas';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
 
 @Resolver('Todo')
-export class TodoResolver {
+export class TodosResolver {
   constructor(private todosService: TodoService) {}
 
   @Query('todos')
+  @UseGuards(GqlAuthGuard)
   async todos(@Args('todoListId') todoListId: number) {
     return this.todosService.findByTodoList(todoListId);
   }
 
   @Query('todo')
+  @UseGuards(GqlAuthGuard)
   async todo(@Args('id') id: number) {
     return this.todosService.findOne(id);
   }
 
   @Mutation('createTodo')
+  @UseGuards(GqlAuthGuard)
   async createTodo(
     @Args('input', new JoiValidationPipe(createTodoSchema)) input: CreateTodoInput,
   ) {
@@ -26,6 +31,7 @@ export class TodoResolver {
   }
 
   @Mutation('updateTodo')
+  @UseGuards(GqlAuthGuard)
   async updateTodo(
     @Args('id') id: number,
     @Args('input', new JoiValidationPipe(updateTodoSchema)) input: UpdateTodoInput,
@@ -34,6 +40,7 @@ export class TodoResolver {
   }
 
   @Mutation('deleteTodo')
+  @UseGuards(GqlAuthGuard)
   async deleteTodo(@Args('id') id: number) {
     return this.todosService.delete(id);
   }
