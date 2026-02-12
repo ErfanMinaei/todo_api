0fde90e46a57dd2a5d16344bb7dec8494b29073a
Erfan <erfanminaei9@gmail.com>
2026-02-12 15:46:59 +0330

    A newly created user has no todo lists, so including them was unnecessary

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,7 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findOne(id: number) {
     return this.prisma.user.findUnique({
@@ -15,21 +15,19 @@ export class UsersService {
   }
 
   async findByUsername(username: string) {
-  return this.prisma.user.findUnique({
-    where: { username },
-    include: { todoLists: true },
-  });
+    return this.prisma.user.findUnique({
+      where: { username },
+    });
   }
 
   async create(input: CreateUserInput) {
     const hashedPassword = await bcrypt.hash(input.password, 10);
-    
+
     return this.prisma.user.create({
       data: {
         ...input,
         password: hashedPassword,
       },
-      include: { todoLists: true },
     });
   }
 }
