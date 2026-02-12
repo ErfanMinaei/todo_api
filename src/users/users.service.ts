11cfcffdbcd910073d75ef55a162266d46cb5723
Erfan <erfanminaei9@gmail.com>
2026-02-12 16:47:31 +0330

    handle error : 'Username already exists'

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserInput } from '../graphql';
 import * as bcrypt from 'bcrypt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class UsersService {
@@ -23,11 +24,20 @@ export class UsersService {
   async create(input: CreateUserInput) {
     const hashedPassword = await bcrypt.hash(input.password, 10);
 
-    return this.prisma.user.create({
-      data: {
-        ...input,
-        password: hashedPassword,
-      },
-    });
+    try {
+      return await this.prisma.user.create({
+        data: {
+          ...input,
+          password: hashedPassword,
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException('Username already exists');
+        }
+      }
+      throw error;
+    }
   }
 }
