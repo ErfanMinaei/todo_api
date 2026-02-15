2c985acfd8863a420ffa5a142590c4c946006fec
Erfan <erfanminaei9@gmail.com>
2026-02-15 15:32:28 +0330

    Reorganize GraphQL schema into modular domain files

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -7,101 +8,96 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export class UpdateTodoListInput {
-  title?: Nullable<string>;
-}
-
-export class CreateUserInput {
-  first_name: string;
-  last_name?: Nullable<string>;
-  username: string;
-  password: string;
-}
-
 export class LoginInput {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
+}
+
+export class UpdateTodoListInput {
+    title?: Nullable<string>;
 }
 
 export class CreateTodoListInput {
-  title: string;
+    title: string;
 }
 
 export class CreateTodoInput {
-  title: string;
-  description: string;
-  deadline: DateTime;
-  todoListId: number;
+    title: string;
+    description: string;
+    deadline: DateTime;
+    todoListId: number;
 }
 
 export class UpdateTodoInput {
-  title?: Nullable<string>;
-  description?: Nullable<string>;
-  is_done?: Nullable<boolean>;
-  deadline?: Nullable<DateTime>;
+    title?: Nullable<string>;
+    description?: Nullable<string>;
+    is_done?: Nullable<boolean>;
+    deadline?: Nullable<DateTime>;
 }
 
-export class User {
-  id: number;
-  first_name: string;
-  last_name?: Nullable<string>;
-  username: string;
-  todoLists: TodoList[];
-}
-
-export class TodoList {
-  id: number;
-  title: string;
-  created_at: DateTime;
-  userId: number;
-  user: User;
-  todos: Todo[];
-}
-
-export class Todo {
-  id: number;
-  title: string;
-  description: string;
-  is_done: boolean;
-  deadline: DateTime;
-  todoListId: number;
-  todoList: TodoList;
+export class CreateUserInput {
+    first_name: string;
+    last_name?: Nullable<string>;
+    username: string;
+    password: string;
 }
 
 export class AuthPayload {
-  token: string;
-  user: User;
-}
-
-export abstract class IQuery {
-  abstract todoLists(): TodoList[] | Promise<TodoList[]>;
-
-  abstract todos(todoListId: number): Todo[] | Promise<Todo[]>;
-
-  abstract todo(id: number): Nullable<Todo> | Promise<Nullable<Todo>>;
+    token: string;
+    user: User;
 }
 
 export abstract class IMutation {
-  abstract createUser(input: CreateUserInput): User | Promise<User>;
+    abstract login(input: LoginInput): AuthPayload | Promise<AuthPayload>;
 
-  abstract login(input: LoginInput): AuthPayload | Promise<AuthPayload>;
+    abstract createTodoList(input: CreateTodoListInput): TodoList | Promise<TodoList>;
 
-  abstract createTodoList(
-    input: CreateTodoListInput,
-  ): TodoList | Promise<TodoList>;
+    abstract updateTodoList(id: number, input: UpdateTodoListInput): TodoList | Promise<TodoList>;
 
-  abstract updateTodoList(
-    id: number,
-    input: UpdateTodoListInput,
-  ): TodoList | Promise<TodoList>;
+    abstract deleteTodoList(id: number): boolean | Promise<boolean>;
 
-  abstract deleteTodoList(id: number): boolean | Promise<boolean>;
+    abstract createTodo(input: CreateTodoInput): Todo | Promise<Todo>;
 
-  abstract createTodo(input: CreateTodoInput): Todo | Promise<Todo>;
+    abstract updateTodo(id: number, input: UpdateTodoInput): Todo | Promise<Todo>;
 
-  abstract updateTodo(id: number, input: UpdateTodoInput): Todo | Promise<Todo>;
+    abstract deleteTodo(id: number): boolean | Promise<boolean>;
 
-  abstract deleteTodo(id: number): boolean | Promise<boolean>;
+    abstract createUser(input: CreateUserInput): User | Promise<User>;
+}
+
+export class TodoList {
+    id: number;
+    title: string;
+    created_at: DateTime;
+    userId: number;
+    user: User;
+    todos: Todo[];
+}
+
+export abstract class IQuery {
+    abstract todoLists(): TodoList[] | Promise<TodoList[]>;
+
+    abstract todos(todoListId: number): Todo[] | Promise<Todo[]>;
+
+    abstract todo(id: number): Nullable<Todo> | Promise<Nullable<Todo>>;
+}
+
+export class Todo {
+    id: number;
+    title: string;
+    description: string;
+    is_done: boolean;
+    deadline: DateTime;
+    todoListId: number;
+    todoList: TodoList;
+}
+
+export class User {
+    id: number;
+    first_name: string;
+    last_name?: Nullable<string>;
+    username: string;
+    todoLists: TodoList[];
 }
 
 export type DateTime = any;
